daca4bdae3643cf11cf53b4065b99530e2a005bc
Landen Danyluk <landendanyluk@gmail.com>
2018-03-27 19:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- cart[item] = Math.floor(Math.random()*100);
+ newItem = Object.new({itemName: item; itemPrice: Math.floor(Math.random()*100)});
+ cart.push(newItem)
  console.log(`${item} has been added to your cart.`)
  return cart
 }
