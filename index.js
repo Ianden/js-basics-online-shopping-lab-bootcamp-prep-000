f309b8a5f36416a82faa5cd83c3ab0c95b11f530
Landen Danyluk <landendanyluk@gmail.com>
2018-03-27 20:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function addToCart(item) {
 
 function viewCart() {
   var response
-  debugger;
   if (cart.length > 0) {
     response = `In your cart, you have ${Object.keys(cart)[0]} at ${Object.values(cart)[0]}`
 
