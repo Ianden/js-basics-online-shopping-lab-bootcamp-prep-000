4eabd3b08142b83d01497a7cc7f0f78bf090e45f
Landen Danyluk <landendanyluk@gmail.com>
2018-03-27 20:44:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ function viewCart() {
   if (cart.length > 0) {
     response = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}`
       if (cart.length > 1) {
+        response = response + ','
         for (var i = 1; i < cart.length - 1; i++) {
-          response = response + `, ${Object.keys(cart[i])} at $${Object.values(cart[i])}`
+          response = response + ` ${Object.keys(cart[i])} at $${Object.values(cart[i])},`
         }
-        response = response + `, and ${Object.keys(cart[cart.length + - 1])} at $${Object.values(cart[cart.length - 1])}`
+        response = response + ` and ${Object.keys(cart[cart.length + - 1])} at $${Object.values(cart[cart.length - 1])}`
       }
       response = response + '.'
       debugger;
