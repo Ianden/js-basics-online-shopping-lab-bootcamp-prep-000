5a93746d0707735ee3392305ae7925923d002295
Landen Danyluk <landendanyluk@gmail.com>
2018-03-27 20:48:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function viewCart() {
   if (cart.length > 0) {
     response = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}`
       if (cart.length > 1) {
-        response = response + ','
+        if (cart.length !== 2) {
+          response = response + ','
+        }
         for (var i = 1; i < cart.length - 1; i++) {
           response = response + ` ${Object.keys(cart[i])} at $${Object.values(cart[i])},`
         }
